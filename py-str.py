97306a63cc307f0eec3845ebbfe3b7f624ff26fc
Clinton <nyaenyaklinton@gmail.com>
2026-03-12 10:45:32 +0300

    python strings operations

---
+++ py-str.py
@@ -0,0 +1,21 @@
+#Python Strings and  Strings Operations
+# String is a sequence of character ensclosed in a single quote or double quotes
+
+name = 'Clinton'
+number = '12345' # a number but it is treated as a string if it is enclosed in quotes
+status = 'True' # a boolean but it is treated as a string if it is enclosed in quotes
+
+# String operations
+# newline character
+
+print("Hello\nWorld") # \n is a newline character that creates a new line
+
+# tab character
+print("Hello\tWorld") # \t is a tab character that creates a tab space
+
+# string concatenation
+first_name = 'Clinton'
+last_name = 'Ombui'
+
+print(first_name + ' ' + last_name) # concatenating first name and last name with a space in between
+
